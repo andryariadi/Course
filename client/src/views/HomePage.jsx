f4185fad08d6ba41a4f8e6729071e687b2a7b81e
Andry Ariadi <andryariadi23@gmail.com>
2023-10-11 22:55:49 +0700

    add testimonial

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,14 +1,29 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Dream from "../assets/dream.png";
-import { kelasTerbaru } from "../data/index";
+import { kelasTerbaru, dataSwiper } from "../data/index";
+import { useNavigate } from "react-router-dom";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleKelas = () => {
+    navigate("/kelas");
+  };
   return (
     <>
       <div className="homepage">
         <header>
           <Container>
-            <Row className="header-box d-flex align-items-center justify-content-center">
+            <Row className="header-box d-flex align-items-center justify-content-center pt-lg-5">
               <Col lg="6">
                 <h1 className="mb-4">
                   Temukan <br /> <span>Bakat Terbaikmu</span> <br /> Bersama Kami!
@@ -53,6 +68,67 @@ export default function HomePage() {
                   </Col>
                 );
               })}
+            </Row>
+            <Row>
+              <Col className="text-center">
+                <button onClick={handleKelas} className="btn btn-primary rounded-5 btn-md">
+                  Class List
+                  <i className="fa-solid fa-chevron-right ms-1"></i>
+                </button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="testimonial py-5">
+          <Container>
+            <Row>
+              <Col>
+                <h1 className="text-center fw-bold my-5">Testimonial</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Swiper
+                slidesPerView={1}
+                spaceBetween={10}
+                pagination={{
+                  clickable: true,
+                }}
+                breakpoints={{
+                  640: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                    spaceBetween: 40,
+                  },
+                  992: {
+                    slidesPerView: 2,
+                    spaceBetween: 50,
+                  },
+                  1200: {
+                    slidesPerView: 3,
+                    spaceBetween: 50,
+                  },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+              >
+                {dataSwiper.map((swiper) => {
+                  return (
+                    <SwiperSlide key={swiper.id} className="shadow rounded-1">
+                      <p className="desc">{swiper.desc}</p>
+                      <div className="people">
+                        <img src={swiper.image} alt="Swiper" />
+                        <div>
+                          <h5 className="mb-1">{swiper.name}</h5>
+                          <p className="fw-bold m-0">{swiper.skill}</p>
+                        </div>
+                      </div>
+                    </SwiperSlide>
+                  );
+                })}
+              </Swiper>
             </Row>
           </Container>
         </div>
